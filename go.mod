578c1cfab5a5de519c2708373336996d8394fd19
Jan Guzman <janfrancisco19@gmail.com>
2019-09-17 16:53:27 +0200

    Working with services files

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Krystian19/cactus-core
 
 go 1.12
+
+require (
+	github.com/golang/protobuf v1.3.2
+	google.golang.org/grpc v1.23.1
+)
--- services/anime.go
+++ services/anime.go
@@ -1,3 +1,11 @@
 package services
 
+import (
+	"context"
 
+	"github.com/Krystian19/cactus-core/proto"
+)
+
+func (s *server) Anime(ctx context.Context, request *proto.Empty) (*proto.Anime, error) {
+	return &proto.Anime{Id: 1, Title: "Boku No Hero Academia"}, nil
+}
