6b983c07399dfe4bf9dd0d044897c1138645f502
Jan Guzman <janfrancisco19@gmail.com>
2019-09-19 20:39:58 +0200

    Working with the release methods

--- services/release.go
+++ services/release.go
@@ -2,18 +2,74 @@ package services
 
 import (
 	"context"
+	"fmt"
+	"log"
 
+	"github.com/Krystian19/cactus-core/models"
 	"github.com/Krystian19/cactus-core/proto"
+	"github.com/jinzhu/gorm"
 )
 
 // Release : Get a single Release based on the provided params
 func (s *Server) Release(ctx context.Context, request *proto.ReleaseRequest) (*proto.ReleaseResponse, error) {
-	return &proto.ReleaseResponse{Release: &proto.Release{Id: 1, Title: "This is a testing title"}}, nil
+	db, err := db()
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	defer db.Close()
+
+	var result models.Release
+	query := db
+
+	if request.Id != 0 {
+		query = query.Where("id = ?", request.Id)
+	}
+
+	if err := query.First(&result).Error; err != nil {
+
+		// If nothing was found
+		if gorm.IsRecordNotFoundError(err) {
+			return &proto.ReleaseResponse{Release: nil}, nil
+		}
+
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return &proto.ReleaseResponse{Release: result.Release}, nil
 }
 
 // Releases : Get a single Releases based on the provided params
 func (s *Server) Releases(ctx context.Context, request *proto.ReleasesRequest) (*proto.ReleasesResponse, error) {
-	return &proto.ReleasesResponse{Releases: []*proto.Release{
-		&proto.Release{Id: 1, Title: "This is a testing title"},
-	}}, nil
+	db, err := db()
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	defer db.Close()
+
+	var result []models.Release
+	query := db
+
+	if request.Query.AnimeId != 0 {
+		query = query.Where("anime_id = ?", request.Query.AnimeId)
+	}
+
+	if err := query.Find(&result).Error; err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	finalRes := []*proto.Release{}
+
+	for i := range result {
+    finalRes[i] = result[i].Release
+  }
+
+	return &proto.ReleasesResponse{Releases: finalRes}, nil
 }
