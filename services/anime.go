46bfb34530e9d43c6c665629cb0d105d8f1e7ca1
Jan Guzman <janfrancisco19@gmail.com>
2019-09-19 13:07:07 +0200

    Working with the Anime resolver

--- services/anime.go
+++ services/anime.go
@@ -3,6 +3,9 @@ package services
 import (
 	"context"
 	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
 
 	"github.com/Krystian19/cactus-core/models"
 	"github.com/Krystian19/cactus-core/proto"
@@ -11,13 +14,22 @@ import (
 // Anime : Get a single Anime based on the provided params
 func (s *Server) Anime(ctx context.Context, request *proto.Empty) (*proto.AnimeResponse, error) {
 	db, err := db()
+
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
+		return nil, err
 	}
+
 	defer db.Close()
 
 	var anime models.Anime
 	if err := db.Where("id = ?", 3).First(&anime).Error; err != nil {
+
+		// If nothing was found
+		if gorm.IsRecordNotFoundError(err) {
+			return &proto.AnimeResponse{Anime: nil}, nil
+		}
+
 		fmt.Println(err)
 	}
 
