9e9f48b95411193dbfe411b1da40cc872cc5897d
Jan Guzman <janfrancisco19@gmail.com>
2019-09-19 11:06:02 +0200

    Setting checks for the env vars

--- bin/main.go
+++ bin/main.go
@@ -16,6 +16,9 @@ func main() {
 		PORT = "9040"
 	}
 
+	// Check for the important env vars to be present
+	EnvVarsCheck()
+
 	listener, err := net.Listen("tcp", ":"+PORT)
 
 	if err != nil {
@@ -30,5 +33,24 @@ func main() {
 
 	if e := srv.Serve(listener); e != nil {
 		panic(e)
+	}
+}
+
+// EnvVarsCheck : Checks that important ENV vars are set
+func EnvVarsCheck() {
+	if os.Getenv("DB_HOST") == "" {
+		panic("DB_HOST env var is not set")
+	}
+
+	if os.Getenv("DB_NAME") == "" {
+		panic("DB_NAME env var is not set")
+	}
+
+	if os.Getenv("DB_USERNAME") == "" {
+		panic("DB_USERNAME env var is not set")
+	}
+
+	if os.Getenv("DB_PASSWORD") == "" {
+		panic("DB_PASSWORD env var is not set")
 	}
 }
