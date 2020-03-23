b177183fff36235d2c75de9f200f5f5ed8558091
Jan Guzman <janfrancisco19@gmail.com>
2020-03-24 00:50:32 +0100

    Adding the service builder struct to the codebase

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,3 +1,25 @@
+use async_trait::async_trait;
+use std::net::SocketAddr;
+use tonic::transport::Server;
+
+use anime::{anime::anime_service_server::AnimeServiceServer, Anime};
 mod anime;
 
 pub struct Services;
+
+#[async_trait]
+pub trait ServicesMethods {
+  async fn build_server(&self, addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+#[async_trait]
+impl ServicesMethods for Services {
+  async fn build_server(&self, addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
+    Server::builder()
+      .add_service(AnimeServiceServer::new(Anime::default()))
+      .serve(addr)
+      .await?;
+
+    Ok(())
+  }
+}
