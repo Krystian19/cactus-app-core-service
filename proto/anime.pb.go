5de81728c8e8a020e6ad9abcbdc536c72429a6a3
Jan Guzman <janfrancisco19@gmail.com>
2020-03-24 15:49:37 +0100

    Fixing the proto generated files field

--- proto/anime.pb.go
+++ proto/anime.pb.go
@@ -24,6 +24,84 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
+type AnimeRequest struct {
+	Id                   string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *AnimeRequest) Reset()         { *m = AnimeRequest{} }
+func (m *AnimeRequest) String() string { return proto.CompactTextString(m) }
+func (*AnimeRequest) ProtoMessage()    {}
+func (*AnimeRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_fd224218081fca6f, []int{0}
+}
+
+func (m *AnimeRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_AnimeRequest.Unmarshal(m, b)
+}
+func (m *AnimeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_AnimeRequest.Marshal(b, m, deterministic)
+}
+func (m *AnimeRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_AnimeRequest.Merge(m, src)
+}
+func (m *AnimeRequest) XXX_Size() int {
+	return xxx_messageInfo_AnimeRequest.Size(m)
+}
+func (m *AnimeRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_AnimeRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_AnimeRequest proto.InternalMessageInfo
+
+func (m *AnimeRequest) GetId() string {
+	if m != nil {
+		return m.Id
+	}
+	return ""
+}
+
+type AnimeReply struct {
+	Anime                *Anime   `protobuf:"bytes,1,opt,name=anime,proto3" json:"anime,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *AnimeReply) Reset()         { *m = AnimeReply{} }
+func (m *AnimeReply) String() string { return proto.CompactTextString(m) }
+func (*AnimeReply) ProtoMessage()    {}
+func (*AnimeReply) Descriptor() ([]byte, []int) {
+	return fileDescriptor_fd224218081fca6f, []int{1}
+}
+
+func (m *AnimeReply) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_AnimeReply.Unmarshal(m, b)
+}
+func (m *AnimeReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_AnimeReply.Marshal(b, m, deterministic)
+}
+func (m *AnimeReply) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_AnimeReply.Merge(m, src)
+}
+func (m *AnimeReply) XXX_Size() int {
+	return xxx_messageInfo_AnimeReply.Size(m)
+}
+func (m *AnimeReply) XXX_DiscardUnknown() {
+	xxx_messageInfo_AnimeReply.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_AnimeReply proto.InternalMessageInfo
+
+func (m *AnimeReply) GetAnime() *Anime {
+	if m != nil {
+		return m.Anime
+	}
+	return nil
+}
+
 type Anime struct {
 	Id                   int64    `protobuf:"varint,1,opt,name=id,proto3" json:"id,omitempty"`
 	Title                string   `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
@@ -38,7 +116,7 @@ func (m *Anime) Reset()         { *m = Anime{} }
 func (m *Anime) String() string { return proto.CompactTextString(m) }
 func (*Anime) ProtoMessage()    {}
 func (*Anime) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fd224218081fca6f, []int{0}
+	return fileDescriptor_fd224218081fca6f, []int{2}
 }
 
 func (m *Anime) XXX_Unmarshal(b []byte) error {
@@ -87,90 +165,10 @@ func (m *Anime) GetUpdatedAt() string {
 	return ""
 }
 
-// The request message containing the user's name.
-type AnimeRequest struct {
-	Name                 string   `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *AnimeRequest) Reset()         { *m = AnimeRequest{} }
-func (m *AnimeRequest) String() string { return proto.CompactTextString(m) }
-func (*AnimeRequest) ProtoMessage()    {}
-func (*AnimeRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fd224218081fca6f, []int{1}
-}
-
-func (m *AnimeRequest) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_AnimeRequest.Unmarshal(m, b)
-}
-func (m *AnimeRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_AnimeRequest.Marshal(b, m, deterministic)
-}
-func (m *AnimeRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_AnimeRequest.Merge(m, src)
-}
-func (m *AnimeRequest) XXX_Size() int {
-	return xxx_messageInfo_AnimeRequest.Size(m)
-}
-func (m *AnimeRequest) XXX_DiscardUnknown() {
-	xxx_messageInfo_AnimeRequest.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_AnimeRequest proto.InternalMessageInfo
-
-func (m *AnimeRequest) GetName() string {
-	if m != nil {
-		return m.Name
-	}
-	return ""
-}
-
-// The response message containing the greetings
-type AnimeReply struct {
-	Anime                *Anime   `protobuf:"bytes,1,opt,name=anime,proto3" json:"anime,omitempty"`
-	XXX_NoUnkeyedLiteral struct{} `json:"-"`
-	XXX_unrecognized     []byte   `json:"-"`
-	XXX_sizecache        int32    `json:"-"`
-}
-
-func (m *AnimeReply) Reset()         { *m = AnimeReply{} }
-func (m *AnimeReply) String() string { return proto.CompactTextString(m) }
-func (*AnimeReply) ProtoMessage()    {}
-func (*AnimeReply) Descriptor() ([]byte, []int) {
-	return fileDescriptor_fd224218081fca6f, []int{2}
-}
-
-func (m *AnimeReply) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_AnimeReply.Unmarshal(m, b)
-}
-func (m *AnimeReply) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_AnimeReply.Marshal(b, m, deterministic)
-}
-func (m *AnimeReply) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_AnimeReply.Merge(m, src)
-}
-func (m *AnimeReply) XXX_Size() int {
-	return xxx_messageInfo_AnimeReply.Size(m)
-}
-func (m *AnimeReply) XXX_DiscardUnknown() {
-	xxx_messageInfo_AnimeReply.DiscardUnknown(m)
-}
-
-var xxx_messageInfo_AnimeReply proto.InternalMessageInfo
-
-func (m *AnimeReply) GetAnime() *Anime {
-	if m != nil {
-		return m.Anime
-	}
-	return nil
-}
-
 func init() {
-	proto.RegisterType((*Anime)(nil), "proto.Anime")
 	proto.RegisterType((*AnimeRequest)(nil), "proto.AnimeRequest")
 	proto.RegisterType((*AnimeReply)(nil), "proto.AnimeReply")
+	proto.RegisterType((*Anime)(nil), "proto.Anime")
 }
 
 func init() {
@@ -178,20 +176,20 @@ func init() {
 }
 
 var fileDescriptor_fd224218081fca6f = []byte{
-	// 205 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x54, 0x8f, 0xcf, 0x4a, 0xc4, 0x30,
-	0x10, 0xc6, 0x69, 0x77, 0x23, 0x74, 0x5c, 0x84, 0x1d, 0x3d, 0x14, 0x41, 0x58, 0x72, 0xda, 0xd3,
-	0x56, 0xea, 0x03, 0x48, 0x5f, 0x21, 0x3e, 0x80, 0xc4, 0x66, 0x0e, 0xc1, 0xfe, 0x89, 0x75, 0x2a,
-	0xf4, 0xed, 0xa5, 0x93, 0xf4, 0xd0, 0x53, 0x26, 0xbf, 0xdf, 0x47, 0xbe, 0x09, 0x9c, 0xc3, 0x34,
-	0xf2, 0x58, 0xd9, 0xc1, 0xf7, 0x74, 0x93, 0x19, 0x95, 0x1c, 0xfa, 0x1b, 0x54, 0xb3, 0x52, 0x7c,
-	0x80, 0xdc, 0xbb, 0x32, 0xbb, 0x64, 0xd7, 0x83, 0xc9, 0xbd, 0xc3, 0x27, 0x50, 0xec, 0xb9, 0xa3,
-	0x32, 0xbf, 0x64, 0xd7, 0xc2, 0xc4, 0x0b, 0xbe, 0x00, 0xb4, 0x13, 0x59, 0x26, 0xf7, 0x69, 0xb9,
-	0x3c, 0x88, 0x2a, 0x12, 0x69, 0x78, 0xd5, 0x73, 0x70, 0x9b, 0x3e, 0x46, 0x9d, 0x48, 0xc3, 0x5a,
-	0xc3, 0x49, 0xca, 0x0c, 0xfd, 0xcc, 0xf4, 0xcb, 0x88, 0x70, 0x1c, 0x6c, 0x4f, 0xd2, 0x5a, 0x18,
-	0x99, 0xf5, 0x2b, 0x40, 0xca, 0x84, 0x6e, 0x41, 0x0d, 0x4a, 0x96, 0x96, 0xc8, 0x7d, 0x7d, 0x8a,
-	0xcb, 0xdf, 0x62, 0x22, 0xaa, 0xfa, 0x3d, 0xbd, 0xfa, 0x41, 0xd3, 0x9f, 0x6f, 0x09, 0xab, 0xed,
-	0x4b, 0x8f, 0xbb, 0x74, 0xec, 0x7c, 0x3e, 0xef, 0x61, 0xe8, 0x96, 0xaf, 0x3b, 0x21, 0x6f, 0xff,
-	0x01, 0x00, 0x00, 0xff, 0xff, 0x1e, 0x98, 0x5e, 0x52, 0x26, 0x01, 0x00, 0x00,
+	// 195 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x2c, 0x28, 0xca, 0x2f,
+	0xc9, 0xd7, 0x4f, 0xcc, 0xcb, 0xcc, 0x4d, 0xd5, 0x03, 0xb3, 0x85, 0x58, 0xc1, 0x94, 0x92, 0x1c,
+	0x17, 0x8f, 0x23, 0x48, 0x34, 0x28, 0xb5, 0xb0, 0x34, 0xb5, 0xb8, 0x44, 0x88, 0x8f, 0x8b, 0x29,
+	0x33, 0x45, 0x82, 0x51, 0x81, 0x51, 0x83, 0x33, 0x88, 0x29, 0x33, 0x45, 0xc9, 0x80, 0x8b, 0x0b,
+	0x2a, 0x5f, 0x90, 0x53, 0x29, 0xa4, 0xc4, 0xc5, 0x0a, 0x36, 0x03, 0xac, 0x80, 0xdb, 0x88, 0x07,
+	0x62, 0x96, 0x1e, 0x44, 0x05, 0x44, 0x4a, 0x29, 0x9b, 0x8b, 0x15, 0xcc, 0x47, 0x32, 0x8a, 0x19,
+	0x64, 0x94, 0x90, 0x08, 0x17, 0x6b, 0x49, 0x66, 0x49, 0x4e, 0xaa, 0x04, 0x13, 0xd8, 0x74, 0x08,
+	0x47, 0x48, 0x96, 0x8b, 0x2b, 0xb9, 0x28, 0x35, 0xb1, 0x24, 0x35, 0x25, 0x3e, 0xb1, 0x44, 0x82,
+	0x19, 0x2c, 0xc5, 0x09, 0x15, 0x71, 0x2c, 0x01, 0x49, 0x97, 0x16, 0xa4, 0xc0, 0xa4, 0x59, 0x20,
+	0xd2, 0x50, 0x11, 0xc7, 0x12, 0x23, 0x7b, 0xa8, 0xf3, 0x83, 0x53, 0x8b, 0xca, 0x32, 0x93, 0x53,
+	0x85, 0xf4, 0x61, 0x96, 0x0b, 0xa3, 0x38, 0x0d, 0xe2, 0x39, 0x29, 0x41, 0x54, 0xc1, 0x82, 0x9c,
+	0xca, 0x24, 0x36, 0xb0, 0x88, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x11, 0x91, 0x98, 0x2a, 0x22,
+	0x01, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
